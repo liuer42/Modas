da53d7b2b9754c1feede26715fe554520b36ebdc
Eric Liu <eliu@my.wctc.edu>
2020-01-20 20:01:54 -0600

    added models

---
+++ Models/IEventRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+namespace Modas.Models
+{
+    public interface IEventRepository
+    {
+        IQueryable<Event> Events { get; }
+        IQueryable<Location> Locations { get; }
+    }
+}
