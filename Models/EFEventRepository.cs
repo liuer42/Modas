a5ec1f2e231e750fae5ca74def3fbefa8a08a744
Eric Liu <eliu@my.wctc.edu>
2020-01-27 18:45:23 -0600

    Create database/migration

---
+++ Models/EFEventRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Modas.Models
+{
+    public class EFEventRepository : IEventRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFEventRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Event> Events => context.Events;
+        public IQueryable<Location> Locations => context.Locations;
+    }
+}
--- Models/Event.cs
+++ Models/Event.cs
@@ -10,9 +10,7 @@ namespace Modas.Models
         public int EventId { get; set; }
         public DateTime TimeStamp { get; set; }
         public bool Flagged { get; set; }
-        // foreign key for location 
         public int LocationId { get; set; }
-        // navigation property
         public Location Location { get; set; }
     }
 }
